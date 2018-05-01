3a5550a8eb209cd2127842ce324ec5d009656cc8
Kelsey Fecho <kfecho30@gmail.com>
2018-05-01 18:59:58 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,7 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: { in: [true, false] }
   with_options if: :released do |r|
     r.validates :release_year, presence: true, numericality: { less_than_or_equal_to: Date.today.year}
+  end
   #validates :release_year, presence: true, if: :released
   #validates :release_year, numericality: { less_than_or_equal_to: Date.today.year}, if: :released
   validates :artist_name, presence: true
