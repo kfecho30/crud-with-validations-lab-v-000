8abe456863dad63b4660f77f870f2278f63ef298
Kelsey Fecho <kfecho30@gmail.com>
2018-05-01 19:04:47 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,13 @@ class SongsController < ApplicationController
   end
 
   def create
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to song_url(@song)
+    else
+      render :new
+    end
   end
 
   def edit
