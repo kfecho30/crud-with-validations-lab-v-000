f1897819d826a21b3ded1c292663a0dc5cd35d22
Kelsey Fecho <kfecho30@gmail.com>
2018-05-01 19:00:23 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,8 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: {scope: :release_year, message: "can only release a named song once per year"}
   validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
   with_options if: :released do |r|
     r.validates :release_year, presence: true, numericality: { less_than_or_equal_to: Date.today.year}
   end
-  #validates :release_year, presence: true, if: :released
-  #validates :release_year, numericality: { less_than_or_equal_to: Date.today.year}, if: :released
-  validates :artist_name, presence: true
 end
